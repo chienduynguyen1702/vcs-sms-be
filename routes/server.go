16dcc06f05dce0569bf5e8f53282b1c7f6b63b03
chienduynguyen1702 <chiennd172002@gmail.com>
2024-06-10 13:49:44 +0700

    fixed flush cache

--- routes/server.go
+++ routes/server.go
@@ -12,10 +12,10 @@ func setupGroupServer(rg *gin.RouterGroup) {
 	rg.GET("/servers", middleware.RequiredAuth, serverController.GetServers)
 	rg.GET("/servers/:id", middleware.RequiredAuth, serverController.GetServerByID)
 	rg.POST("/servers", middleware.RequiredAuth, serverController.CreateServer, serverController.FlushCache)
-	rg.PUT("/servers/:id", middleware.RequiredAuth, serverController.UpdateServer)
+	rg.PUT("/servers/:id", middleware.RequiredAuth, serverController.UpdateServer, serverController.FlushCache)
 	rg.DELETE("/servers/:id", middleware.RequiredAuth, serverController.DeleteServer, serverController.FlushCache)
 
-	rg.GET("/servers/archived", middleware.RequiredAuth, serverController.GetArchivedServer, serverController.FlushCache)
+	rg.GET("/servers/archived", middleware.RequiredAuth, serverController.GetArchivedServer)
 	rg.PATCH("/servers/:id/archive", middleware.RequiredAuth, serverController.ArchiveServer, serverController.FlushCache)
 	rg.PATCH("/servers/:id/restore", middleware.RequiredAuth, serverController.Restore, serverController.FlushCache)
 
