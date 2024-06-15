ec932eebe9df32833518b829ccc2c84873509873
chiennd172002 <chiennd172002@gmail.com>
2024-06-15 21:12:14 +0700

    set default file path

--- mail/gmail.go
+++ mail/gmail.go
@@ -1,6 +1,15 @@
 package main
 
-import "gopkg.in/gomail.v2"
+import (
+	"time"
+
+	"gopkg.in/gomail.v2"
+)
+
+const (
+	DEFAULT_DIR              = "/reports"
+	DEFAULT_PREFIX_FILE_NAME = "VCS-SMS-Report-"
+)
 
 type GmailService struct {
 	Dialer *gomail.Dialer
@@ -16,10 +25,16 @@ func InitGmailService(mail, pass string) *GmailService {
 		Pass:   pass,
 	}
 }
-// send file at filePath to toMail
+
+// send file at filePath to toMail, default filePath is /reports/VCS-SMS-Report-<today>.xlsx
 func (m *GmailService) SendEmail(filePath string, toMail string) error {
+
+	if filePath == "" {
+		filePath = m.DefaultFilePath()
+	}
+
 	msg := gomail.NewMessage()
-	msg.SetHeader("From", "no-reply-vcs@mail.vn")
+	msg.SetHeader("From", "chienduynguyen1702@mail.com")
 	msg.SetHeader("To", toMail)
 	msg.SetHeader("Subject", "Statistical Servers Report")
 	msg.SetBody("text/plain", "Here is the report you requested.")
@@ -29,4 +44,10 @@ func (m *GmailService) SendEmail(filePath string, toMail string) error {
 		return err
 	}
 	return nil
+}
+
+func (m *GmailService) DefaultFilePath() string {
+	todayString := time.Now().Format("2006-01-02")
+
+	return DEFAULT_DIR + "/" + DEFAULT_PREFIX_FILE_NAME + todayString + ".xlsx"
 }
